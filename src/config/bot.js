cc8a361d8649fbaa3097b672889ae2bcbf3ee037
Xavi6929 <zubkoksawery14@gmail.com>
2026-05-07 18:39:05 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "GRUPA OPOROWCY (Farming & Friends Community)",
         // Activity type number (0 = Playing).
         type: 0, 
       },
